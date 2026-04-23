d28bf98eaddba510f142a7d1f6dc3322193e5538
MyRainProgrammer <renzoppy573@gmail.com>
2026-04-23 22:33:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ import express from 'express';
 import chalk from 'chalk';
 import morgan from 'morgan';
 import debug from 'debug';
+import path from 'path;'
 const debugapp = debug('app');
 const app = express();
 const port = 5000;
 
 app.use(morgan('combind'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
 
     res.send('Hello World!!!!!');
 
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     debugapp("Listening on port" + chalk.green(port));
 })
