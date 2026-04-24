c4f8401e7535055041f2017c4bc3a977faccc237
MyRainProgrammer <renzoppy573@gmail.com>
2026-04-24 21:12:39 +0700

    update structure

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import chalk from 'chalk';
 import morgan from 'morgan';
 import debug from 'debug';
 import path from 'path';
+import ejs from 'ejs';
 const debugapp = debug('app');
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT;
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -14,13 +15,15 @@ const __dirname = dirname(__filename);
 
 app.use(morgan('combind'));
 app.use(express.static(path.join(__dirname,"/public/")));
+app.set("views","./src/views");
+app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
 
-    res.send('Hello World!!!!!');
+    res.render('index',{username: 'Sugar mommy', customers: ["Witchayut","Witchayada","Weerapon","Nicchanan"]});
 
 })
 
-app.listen(port, () => {
-    debugapp("Listening on port" + chalk.green(port));
+app.listen(PORT, () => {
+    debugapp("Listening on port" + ":" + chalk.green(PORT));
 })
