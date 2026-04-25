b23161ab8cb8dd7fef9bda1df0e5a39f4d26628b
MyRainProgrammer <renzoppy573@gmail.com>
2026-04-25 22:34:34 +0700

    fix bug

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ app.set("views","./src/views");
 app.set("view engine", "ejs")
 
 productRouter.route("/").get((req,res) =>{
-    res.render("products",
+    res.render("products",{
         products,
+        }
     );
 });
 
