94736aef517ae3ecf54abca1c4f3aeef032274b3
MyRainProgrammer <renzoppy573@gmail.com>
2026-04-26 16:33:05 +0700

    change and update feature and fix bug

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const app = express();
 const PORT = process.env.PORT;
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-const productRouter = express.Router();
-import products from './data/products.json' with {type: "json"}
+import productsRouter from './src/router/productsRouter.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,21 +19,7 @@ app.use(express.static(path.join(__dirname,"/public/")));
 app.set("views","./src/views");
 app.set("view engine", "ejs")
 
-productRouter.route("/").get((req,res) =>{
-    res.render("products",
-        products,
-    );
-});
-
-productRouter.route("/:id").get((req,res) =>{
-    const id = req.params.id;
-    res.render("product",{
-        product: products[id],
-    }
-    );
-});
-
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/", (req, res) => {
 
---
+++ src/router/productsRouter.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const productsRouter = express.Router();
+import products from '../data/products.json' with {type: "json"}
+productsRouter.route("/").get((req,res) =>{
+    res.render("products",{
+        products,
+    }
+    );
+});
+
+productsRouter.route("/:id").get((req,res) =>{
+    const id = req.params.id;
+    res.render("product",{
+        product: products[id],
+    })
+});
+
+export default productsRouter;
